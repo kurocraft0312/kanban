889afd26360e249b379e3fda42f3f8b6c9e0c7fd
kurocraft0312 <kurocraft0312@gmail.com>
2019-06-21 19:40:42 +0900

    ルート定義をしました。

--- kanban/routes/web.php
+++ kanban/routes/web.php
@@ -11,9 +11,18 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//リスト一覧
+Route::get('/','ListingsController@index');
+//リスト新規画面
+Route::get('new','ListingsController@new')->name('new');
+//リスト新規処理
+Route::post('/listings','ListingsController@store');
+//リスト更新画面
+Route::get('/listingsedit/{listing_id}','ListingsController@edit');
+//リスト更新処理
+Route::post('/listing/edit','ListingsController@update');
+//リスト削除処理
+Route::get('listingsdelete/{listing_id}','ListingsController@destroy');
 
 Auth::routes();
 
